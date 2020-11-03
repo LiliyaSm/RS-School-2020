b4c9deaaf82040bd3ec08a9619f441ab156ccc71
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2020-11-03 13:58:19 +0300

    feat: add moves counter

--- gem-puzzle/src/index.js
+++ gem-puzzle/src/index.js
@@ -4,13 +4,11 @@ import Timer from "./utils/timer.js"; // creates canvas field
 
 const body = document.querySelector("body");
 
-
 const SIZE = 80; //width, height
 const minShuffle = 100;
 const maxShuffle = 300;
 
 const DRAG_SENSITIVITY = 6;
-
 
 body.setAttribute("class", "container-fluid");
 
@@ -21,6 +19,8 @@ let fourth_row = create("div", ["row"], body);
 
 let beginAgain = create("button", ["beginAgain"], first_row);
 let select = create("select", ["select"], fourth_row);
+let counter = create("span", ["counter"], fourth_row);
+counter.textContent = 0;
 
 let option1 = create(
     "option",
@@ -51,10 +51,9 @@ class Game {
         this.canvas = null;
         this.rect = null; // abs. size of element
 
-        this.myInterval = null;
+        this.moveCounter = 0;
 
         this.timer = null;
-
 
         this.drag = {
             started: false,
@@ -70,7 +69,7 @@ class Game {
         this.tileRendering = new createField();
         this.tileRendering.init(SIZE, this.PUZZLE_DIFFICULTY);
         this.canvas = this.tileRendering.canvas;
-        this.rect = this.canvas.getBoundingClientRect(); // abs. size of element
+        third_row.appendChild(this.canvas);
 
         select.addEventListener("change", (e) => this.logValue(e));
 
@@ -78,7 +77,6 @@ class Game {
         this.timer = new Timer(time);
         this.timer.start();
 
-        third_row.appendChild(this.canvas);
         //updates every 20th millisecond (50 times per second)
         setInterval(() => {
             this.tileRendering.createTiles(
@@ -89,6 +87,7 @@ class Game {
                 this.drag.y
             );
         }, 20);
+        this.rect = this.canvas.getBoundingClientRect(); // abs. size of element. After loading ALL DOM elements!
     }
 
     restart() {
@@ -97,12 +96,28 @@ class Game {
         this.tileRendering.init(SIZE, this.PUZZLE_DIFFICULTY);
         this.canvas = this.tileRendering.canvas;
         this.rect = this.canvas.getBoundingClientRect(); // abs. size of element
+
+        this.moveCounter = 0;
+        counter.textContent = this.moveCounter;
+
+        this.resetCounter();
+
         this.timer.resetTimer();
     }
 
     logValue(e) {
         this.PUZZLE_DIFFICULTY = Number(e.target.value);
         this.restart();
+    }
+
+    increaseCounter() {
+        ++this.moveCounter;
+        counter.textContent = this.moveCounter;
+    }
+
+    resetCounter() {
+        this.moveCounter = 0;
+        counter.textContent = this.moveCounter;
     }
 
     createWinMap() {
@@ -156,11 +171,9 @@ class Game {
     }
 
     handleClick(event) {
-        let rect = this.canvas.getBoundingClientRect(); // abs. size of element
-        // console.log(rect);
         //mouse position subtracted from the parent element's offset position, mouse position you are getting is relative to the client window
-        let x = event.clientX - rect.left;
-        let y = event.clientY - rect.top;
+        let x = event.clientX - this.rect.left;
+        let y = event.clientY - this.rect.top;
         let col = Math.floor(x / SIZE);
         let row = Math.floor(y / SIZE);
 
@@ -176,6 +189,8 @@ class Game {
             (rowEmptyTile + 1 === row || rowEmptyTile - 1 === row)
         ) {
             // shift in column
+            this.increaseCounter();
+
             if (rowEmptyTile + 1 === row) {
                 this.down(emptyTilePosition);
             } else {
@@ -186,6 +201,8 @@ class Game {
             (colEmptyTile + 1 === col || colEmptyTile - 1 === col)
         ) {
             // shift in row
+            this.increaseCounter();
+
             if (colEmptyTile + 1 === col) {
                 this.right(emptyTilePosition);
             } else {
@@ -228,7 +245,9 @@ class Game {
 
     beginAgainFunc() {
         this.shuffleArray = this.shuffleTiles(this.winMap);
+        this.resetCounter();
         this.timer.resetTimer();
+
     }
 
     myMove(e) {
@@ -252,6 +271,8 @@ class Game {
 
         let rect = this.canvas.getBoundingClientRect(); // abs. size of element
         this.rect = rect;
+        // let rect = this.rect;
+
         console.log(rect);
         //mouse position subtracted from the parent element's offset position, mouse position you are getting is relative to the client window
         let x = event.clientX - rect.left;
@@ -275,8 +296,8 @@ class Game {
         }
         console.log(position);
 
-        this.drag.x = event.clientX - this.rect.left;
-        this.drag.y = event.clientY - this.rect.top;
+        this.drag.x = event.clientX - rect.left;
+        this.drag.y = event.clientY - rect.top;
         this.drag.position = position;
         this.canvas.onmousemove = (e) => this.myMove(e);
         this.canvas.onmouseup = (e) => this.myUp(e);
@@ -304,6 +325,7 @@ class Game {
                 this.drag.position
             ];
             this.shuffleArray[this.drag.position] = 0;
+            // this.increaseCounter();
         }
 
         this.drag.started = false;
