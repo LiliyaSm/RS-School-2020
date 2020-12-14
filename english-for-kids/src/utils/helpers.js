207747ba7e66576ebfdf06c24afe625d107fa759
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2020-12-14 23:06:57 +0300

    refactor: change comparer

--- english-for-kids/src/utils/helpers.js
+++ english-for-kids/src/utils/helpers.js
@@ -1,7 +1,7 @@
 // source: https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
 const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-// eslint-disable-next-line
 const comparer = (idx, ascOrder) => (a, b) => {
+  // eslint-disable-next-line
   const isNumber = (v1, v2) => v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2);
   const valueA = getCellValue(ascOrder ? a : b, idx);
   const valueB = getCellValue(ascOrder ? b : a, idx);
